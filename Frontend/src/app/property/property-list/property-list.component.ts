27f59346816d9da6be1fd10a4eedf1854eb23abd
Azamat <azamat.khamidov@soti.net>
2023-01-23 09:20:09 -0500

    Transferred hard coded data to a json file and used a server to retrieve data at runtime

--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HousingService } from 'src/app/services/housing.service';
 
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
   styleUrls: ['./property-list.component.css']
 })
-export class PropertyListComponent {
+export class PropertyListComponent implements OnInit {
+
+  properties: any;
+
+  constructor(private housingService: HousingService) { }
+
+  ngOnInit(): void {
+    this.housingService.getAllProperties().subscribe(
+      data=>{
+        this.properties=data;
+        console.log(data);
+      }, error=>console.log(error)
+    )
+  }
 
 }
