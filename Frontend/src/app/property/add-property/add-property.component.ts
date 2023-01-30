7a8c83258cf486b20dbca8362b405889f51ff95f
Azamat Khamidov <azamat.khamidov@mail.utoronto.ca>
2023-01-30 18:33:25 -0500

    Improved the list property page by adding new fields in the form

--- Frontend/src/app/property/add-property/add-property.component.ts
+++ Frontend/src/app/property/add-property/add-property.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router'
 import { NgForm } from '@angular/forms'
+import { TabsetComponent } from 'ngx-bootstrap/tabs/public_api'
+import { IProperty } from '../IProperty.interface';
 
 @Component({
   selector: 'app-add-property',
@@ -9,6 +11,19 @@ import { NgForm } from '@angular/forms'
 })
 export class AddPropertyComponent implements OnInit {
   @ViewChild('Form') addPropertyForm: NgForm;
+  @ViewChild('formTabs') formTabs: TabsetComponent;
+
+  propertyTypes: Array<string> = ['House', 'Apartment', 'Duplex']
+  furnishTypes: Array<string> = ['Fully', 'Semi', 'Unfurnished']
+
+  propertyView: IProperty = {
+    Id: null,
+    Name: '',
+    Price: null,
+    SellRent: null,
+    Type: null
+  };
+
   constructor(private router: Router) { }
 
   ngOnInit() {
@@ -20,6 +35,12 @@ export class AddPropertyComponent implements OnInit {
 
   onSubmit() {
     console.log(this.addPropertyForm);
+  }
+
+  selectTab(tabId: number) {
+    if (this.formTabs.tabs[tabId]) {
+      this.formTabs.tabs[tabId].active = true;
+    }
   }
 
 }
