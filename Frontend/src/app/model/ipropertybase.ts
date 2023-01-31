82da1a277400227e45a12990899f4486dad22592
Azamat Khamidov <azamat.khamidov@mail.utoronto.ca>
2023-01-31 11:13:06 -0500

    Edited add-property and property-card components to improve the list property page and bind property card on the right to the add property form on the left

---
+++ Frontend/src/app/model/ipropertybase.ts
@@ -0,0 +1,13 @@
+export interface IPropertyBase {
+  Id: number;
+  SellRent: number;
+  Name: String;
+  FType: String;
+  PType: String;
+  Price: number;
+  BHK: number;
+  BuiltArea: number;
+  City: String;
+  RTM: number;
+  Image?: string;
+}
