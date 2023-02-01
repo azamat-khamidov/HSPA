8df331ffb9d444c1f0bb3b9546e45f797d063600
Azamat Khamidov <azamat.khamidov@mail.utoronto.ca>
2023-02-01 10:27:03 -0500

    Created WebAPI project

---
+++ WebApi/Controllers/CityController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Atlanta", "New York", "Chicago" };
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "Atlanta";
+        }
+    }
+}
