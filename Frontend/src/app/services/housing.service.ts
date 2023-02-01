037c3b7c17ccdaf3a8a2299c2ce21ab6c427e517
Azamat Khamidov <azamat.khamidov@mail.utoronto.ca>
2023-01-31 20:07:45 -0500

    Migrated fetching local storage data to a service and implemented the functionality to save multiple properties

--- Frontend/src/app/services/housing.service.ts
+++ Frontend/src/app/services/housing.service.ts
@@ -17,6 +17,15 @@ export class HousingService {
     return this.http.get('data/properties.json').pipe(
       map(data => {
       const propertiesArray: Array<IPropertyBase> = [];
+      const localProperties = JSON.parse(localStorage.getItem('newProp'));
+
+      if (localProperties) {
+        for (const id in localProperties) {
+          if (localProperties.hasOwnProperty(id) && localProperties[id].SellRent === SellRent) {
+            propertiesArray.push(localProperties[id]);
+          }
+        }
+      }
 
       for (const id in data) {
         if (data.hasOwnProperty(id) && data[id].SellRent === SellRent) {
@@ -30,6 +39,22 @@ export class HousingService {
     return this.http.get<IProperty[]>('data/properties.json');
   }
   addProperty(property: Property) {
-    localStorage.setItem('newProp', JSON.stringify(property));
+    let newProp = [property];
+
+    if (localStorage.getItem('newProp')) {
+      newProp = [property, ...JSON.parse(localStorage.getItem('newProp'))];
+    }
+
+    localStorage.setItem('newProp', JSON.stringify(newProp));
+  }
+
+  newPropID() {
+    if (localStorage.getItem('PID')) {
+      localStorage.setItem('PID', String(+localStorage.getItem('PID') + 1));
+      return +localStorage.getItem('PID');
+    } else {
+      localStorage.setItem('PID', '101');
+      return 101;
+    }
   }
 }
